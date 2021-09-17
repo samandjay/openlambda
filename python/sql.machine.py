002c9970e8985ab84a2eda0bf200a55e46168a04
Ubuntu <ubuntu@ip-172-31-11-143.eu-central-1.compute.internal>
2021-09-17 14:04:26 +0000

    no comments

--- python/sql.machine.py
+++ python/sql.machine.py
@@ -1,20 +1,7 @@
-##let symbols = (size,fidx) => `(select 0 as x${fidx} union ` + Array.from({length: size-1}, (_,idx) => `select ${idx + 1}`).join(' union ') + ') d'
-##let fields = (size, sep, alphabetSize) => Array.from({length: size}, (_,idx) => symbols(alphabetSize,idx) + idx).join(sep)
-##let window = (size, start) => ` limit ${size} offset ${start}`
-
-##let machine = (width, height, start, sep, alphabetSize) => 'select * from\n' + fields(width, sep, alphabetSize) + '\n'  + window(height, start)
-
-
-##let [width, height, start, sep, alphabetSize] = [8, 1024, 0, ',\n',2]
-
-##console.log( machine( width, height, start, sep, alphabetSize ) )
-
-
 symbols = lambda size, fidx : '(select 0 as x{} union '.format(fidx) + ' union '.join(['select {}'.format(i+1) for i in range(size-1)])  + ') d'
 fields = lambda size, sep, alphabetSize : sep.join([ '{}{}'.format(symbols(alphabetSize, i), i) for i in range(size) ])
 window = lambda size, start : ' limit {} offset {}'.format(size, start)
 machine = lambda width, height, start, sep, alphabetSize : 'select * from\n' + fields(width, sep, alphabetSize) + '\n'  + window(height, start)
-
 
 width, height, start, sep, alphabetSize = 8, 1024, 0, ',\n',2
 
