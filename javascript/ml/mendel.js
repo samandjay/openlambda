6f9f1675562ca4b1e52faaa62844f70c8406e973
Ubuntu <ubuntu@ip-172-31-11-143.eu-central-1.compute.internal>
2021-09-18 19:20:56 +0000

    ldm m

---
+++ javascript/ml/mendel.js
@@ -0,0 +1,38 @@
+// modes od mutation
+//Observe that these are very simple mutation modes.
+//Biologically one type of mutation is more common that other (transitions over trnasversions)
+
+// random
+let random = bases => Math.max(1, Math.floor(Math.random()*bases))
+//cyclic
+let cyclic = (b, bases) =>  (b + 1) % bases + 1
+
+let mutate = (b, bases) => random( bases)
+let mutateRow = (row, bases) => row.map( b => b!=0?mutate(b, bases):b)
+
+let mutation = genome => ({
+			bases: genome.bases,
+			rows: genome.rows,
+			cols: genome.cols,
+			data: genome.data.map(r => mutateRow(r, genome.bases))
+			})
+
+let genome = {
+	bases : 4,
+	rows : 6,
+	cols : 6,
+	data :  [
+		[1,1,0,0,0,0],
+		[1,3,0,0,0,0],
+		[0,0,3,2,0,0],
+		[0,0,1,1,0,0],
+		[0,0,0,0,1,1],
+		[0,0,0,0,3,2]
+	]
+}
+
+let generations = 100
+for(let gen = 1; gen <= generations; gen++){
+	genome = mutation( genome )
+	console.log('generation',gen,'\n', genome )
+}
